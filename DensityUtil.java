8f37d8756ddec8184cdff378bd5921a22b3022d3
廖常立 <leo917196100@gmail.com>
2017-03-21 16:31:28 +0800

    权限 显示器

--- DensityUtil.java
+++ DensityUtil.java
@@ -1,65 +1,45 @@
-package coschat.wecanstudio.coschat.utils;
+package com.willplus.leo.stronger.Utills;
 
 import android.content.Context;
 import android.util.TypedValue;
 
 /**
- * 常用单位转换的辅助类
+ * Created by changliliao on 2017/3/20.
  */
-public class DensityUtil
-{
-    private DensityUtil()
-    {
-        /** cannot be instantiated **/
+
+public class DensityUtil {
+    private DensityUtil() {
         throw new UnsupportedOperationException("cannot be instantiated");
     }
 
     /**
      * dp转px
-     *
-     * @param context
-     * @param dpVal
-     * @return
      */
-    public static int dp2px(Context context, float dpVal)
-    {
+    public static int dp2px(Context context, int dpVal) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
                 dpVal, context.getResources().getDisplayMetrics());
     }
 
     /**
      * sp转px
-     *
-     * @param context
-     * @return
      */
-    public static int sp2px(Context context, float spVal)
-    {
+    public static int sp2px(Context context, float spVal) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
                 spVal, context.getResources().getDisplayMetrics());
     }
 
     /**
      * px转dp
-     *
-     * @param context
-     * @param pxVal
-     * @return
      */
-    public static float px2dp(Context context, float pxVal)
-    {
+    public static float px2dp(Context context, float pxVal) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (pxVal / scale);
     }
+
     /**
      * px转sp
-     *
-     * @param pxVal
-     * @return
      */
-    public static float px2sp(Context context, float pxVal)
-    {
+    public static float px2sp(Context context, float pxVal) {
         return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
     }
-
 }
---
+++ PermissionUtils.java
@@ -0,0 +1,17 @@
+package com.willplus.leo.stronger.Utills;
+
+import android.content.Context;
+
+/**
+ * Created by changliliao on 2017/2/12.
+ */
+
+public class PermissionUtils{
+    Context mContext;
+
+    PermissionUtils(Context context){
+        mContext=context;
+    }
+
+
+}
